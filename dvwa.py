bc0e35f46cc6063890c49fb30a78408f471140f1
yukikazenanodesu <31941796+yukikazenanodesu@users.noreply.github.com>
2021-12-14 23:44:58 +0800

    Update dvwa.py

--- dvwa.py
+++ dvwa.py
@@ -14,8 +14,7 @@ class windw(QMainWindow):
     Forged = None
 
     def wdawdawdwasdw(self):
-        selected_row = self.info_tree.currentItem().text(0)  #当前选择的编号
-        #表格停止追踪更新
+        selected_row = self.info_tree.currentItem().text(0)
         if selected_row and selected_row.isdigit():
             self.timer.stop()
             self.wdawdawdaw((int)(selected_row))
@@ -35,7 +34,7 @@ class windw(QMainWindow):
         self.set_hex_text(hex_dump)
     def dwadwadwawdaw(self):
         card = self.choose_nicbox.currentText()
-        self.netNic.setText('当前网卡：' + card)
+        self.netNic.setText('here net card：' + card)
         if (card == 'All'):
             a = None
         elif waawdadw == 'Windows':
@@ -49,7 +48,6 @@ class windw(QMainWindow):
         self.hexBrowser.setText(text)
     def one(self):
         if self.core.stop_flag:
-            # 重新开始清空面板内容
             self.info_tree.clear()
             self.treeWidget.clear()
             self.set_hex_text("")
@@ -84,7 +82,6 @@ class windw(QMainWindow):
         self.action_update.setDisabled(True)
         self.timer.stop()
     def forew(self):
-        # 重新开始清空面板内容
         self.timer.stop()
         self.core.mcw2(self.dwadwadwawdaw(), self.Filter.text())
         self.info_tree.clear()
@@ -110,9 +107,8 @@ class windw(QMainWindow):
             exit()
 
         if self.core.start_flag or self.core.pause_flag:
-            # 没有停止抓包
             reply = QMessageBox.question(
-                self, 'Message', "是否停止保存?\n若不保存东西将会丢失",
+                self, 'Message', "save or not save",
                 QMessageBox.Save | QMessageBox.Close | QMessageBox.Cancel,
                 QMessageBox.Cancel)
             if reply == QMessageBox.Cancel:
@@ -126,7 +122,7 @@ class windw(QMainWindow):
                 close_to_do()
         elif self.core.stop_flag and not self.core.save_flag:
             reply = QMessageBox.question(
-                self, 'Message', "是否停止保存?\n若不保存东西将会丢失",
+                self, 'Message', "save or not save",
                 QMessageBox.Save | QMessageBox.Close | QMessageBox.Cancel,
                 QMessageBox.Cancel)
             if reply == QMessageBox.Cancel:
@@ -137,7 +133,7 @@ class windw(QMainWindow):
             else:
                 close_to_do()
         elif self.core.save_flag or not self.core.start_flag:
-            reply = QMessageBox.question(self, 'Message', "退出不?",
+            reply = QMessageBox.question(self, 'Message', "queite?",
                                          QMessageBox.Yes | QMessageBox.No,
                                          QMessageBox.No)
             if reply == QMessageBox.Yes:
@@ -149,38 +145,26 @@ class windw(QMainWindow):
         pixmap = QPixmap("img/background.jpg")
         painter.drawPixmap(self.rect(), pixmap)
     def wdid(self):
-        self.setWindowTitle("抓包器")
+        self.setWindowTitle("hacker")
         self.resize(950, 580)
-
-        #设置程序图标
         icon = QIcon()
         icon.addPixmap(QPixmap("img/1605833136708.jpeg"), QIcon.Normal, QIcon.Off)
         self.setWindowIcon(icon)
         self.setIconSize(QSize(20, 20))
-        #中间布局，设为透明
         self.centralWidget = QWidget(self)
         self.centralWidget.setStyleSheet("background:transparent;")
-
-        #栅栏布局，使得窗口自适应
         self.gridLayout = QGridLayout(self.centralWidget)
         self.gridLayout.setContentsMargins(0, 0, 0, 0)
         self.gridLayout.setSpacing(7)
-
-        #顶部控件布局
         self.horizontalLayout = QHBoxLayout()
         self.horizontalLayout.setContentsMargins(11, 2, 11, 1)
         self.horizontalLayout.setSpacing(20)
-
-        #三个显示区布局
         self.verticalLayout = QVBoxLayout()
         self.verticalLayout.setContentsMargins(11, 0, 3, 11)
         self.verticalLayout.setSpacing(7)
-
-        # 初始主窗口字体
         font = QFont()
         with open('data.json', 'r') as file_obj:
-            '''读取json文件'''
-            old_font = json.load(file_obj)  # 返回列表数据，也支持字典
+            old_font = json.load(file_obj)
         if old_font["font"]:
             font.setFamily(old_font["font"])
             font.setPointSize(int(old_font["size"]))
@@ -195,17 +179,13 @@ class windw(QMainWindow):
             with open('data.json', 'w') as file_obj:
                 '''写入json文件'''
                 json.dump(old_font, file_obj)
-
-        #数据包显示框
         self.info_tree = QTreeWidget(self.centralWidget)
         self.info_tree.setFrameStyle(QFrame.Box | QFrame.Plain)
         self.info_tree.setAutoScroll(True)
         self.info_tree.setRootIsDecorated(False)
         self.info_tree.setFont(font)
-        self.info_tree.setColumnCount(7)  #设置表格为7列
-        #固定行高，取消每次刷新所有行，避免更新数据时不流畅
+        self.info_tree.setColumnCount(7)
         self.info_tree.setUniformRowHeights(True)
-        #设置表头
         self.info_tree.headerItem().setText(0, "No.")
         self.info_tree.headerItem().setText(1, "Time")
         self.info_tree.headerItem().setText(2, "Source")
@@ -226,13 +206,10 @@ class windw(QMainWindow):
             self.info_tree.headerItem().setBackground(i,
                                                       QBrush(QColor(Qt.white)))
         self.info_tree.setSelectionBehavior(
-            QTreeWidget.SelectRows)  #设置选中时为整行选中
-        self.info_tree.setSelectionMode(QTreeWidget.SingleSelection)  #设置只能选中一行
-        """显示排序图标"""
+            QTreeWidget.SelectRows)
+        self.info_tree.setSelectionMode(QTreeWidget.SingleSelection)
         self.info_tree.header().setSortIndicatorShown(True)
         self.info_tree.clicked.connect(self.wdawdawdwasdw)
-
-        #数据包详细内容显示框
         self.treeWidget = QTreeWidget(self.centralWidget)
         self.treeWidget.setAutoScroll(True)
         self.treeWidget.setTextElideMode(Qt.ElideMiddle)
@@ -240,36 +217,26 @@ class windw(QMainWindow):
         self.treeWidget.setStyleSheet("background:transparent; color:white;")
         self.treeWidget.header().hide()
         self.treeWidget.setFont(font)
-        # 设为只有一列
         self.treeWidget.setColumnCount(1)
         self.treeWidget.setFrameStyle(QFrame.Box | QFrame.Plain)
-
-        #hex显示区域
         self.hexBrowser = QTextBrowser(self.centralWidget)
         self.hexBrowser.setText("")
         self.hexBrowser.setFont(font)
         self.hexBrowser.setStyleSheet("background:transparent;  color:white;")
         self.hexBrowser.setFrameStyle(QFrame.Box | QFrame.Plain)
-
-        # 允许用户通过拖动三个显示框的边界来控制子组件的大小
         self.splitter = QSplitter(Qt.Vertical)
         self.splitter.addWidget(self.info_tree)
         self.splitter.addWidget(self.treeWidget)
         self.splitter.addWidget(self.hexBrowser)
         self.verticalLayout.addWidget(self.splitter)
-
         self.gridLayout.addLayout(self.verticalLayout, 1, 0, 1, 1)
-
-        #过滤器输入框
         self.Filter = QLineEdit(self.centralWidget)
-        self.Filter.setPlaceholderText("Apply a capture filter … ")
+        self.Filter.setPlaceholderText("net work???? … ")
         self.Filter.setStyleSheet("background:white")
         self.Filter.setFont(font)
         self.horizontalLayout.addWidget(self.Filter)
-
-        #过滤器按钮
         self.FilterButton = QPushButton(self.centralWidget)
-        self.FilterButton.setText("开始")
+        self.FilterButton.setText("start")
         icon1 = QIcon()
         icon1.addPixmap(QPixmap("img/redygo.jpg"), QIcon.Normal, QIcon.Off)
         self.FilterButton.setIcon(icon1)
@@ -277,33 +244,24 @@ class windw(QMainWindow):
         self.FilterButton.setStyleSheet("background:white")
         self.FilterButton.clicked.connect(self.one)
         self.horizontalLayout.addWidget(self.FilterButton)
-        """
-           网卡选择框
-        """
         self.choose_nicbox = QComboBox(self.centralWidget)
         self.choose_nicbox.setFont(font)
         self.choose_nicbox.setStyleSheet("background:white; color:black;")
         self.horizontalLayout.addWidget(self.choose_nicbox)
-
         self.horizontalLayout.setStretch(0, 8)
         self.horizontalLayout.setStretch(1, 1)
         self.horizontalLayout.setStretch(2, 4)
         self.gridLayout.addLayout(self.horizontalLayout, 0, 0, 1, 1)
-        """初始网卡复选框"""
         row_num = len(keys)
         self.choose_nicbox.addItem("All")
         for i in range(row_num):
             self.choose_nicbox.addItem(keys[i])
 
         self.setCentralWidget(self.centralWidget)
-        """
-           顶部菜单栏
-        """
         self.menuBar = QMenuBar(self)
         self.menuBar.setGeometry(QRect(0, 0, 0,0))
         self.menuBar.setAccessibleName("")
         self.menuBar.setDefaultUp(True)
-        #顶部工具栏
         self.mainToolBar = QToolBar(self)
         self.addToolBar(Qt.TopToolBarArea, self.mainToolBar)
         self.statusBar = QStatusBar(self)
@@ -314,46 +272,38 @@ class windw(QMainWindow):
         icon2 = QIcon()
         icon2.addPixmap(QPixmap("img/start.png"), QIcon.Normal, QIcon.Off)
         self.start_action.setIcon(icon2)
-        self.start_action.setText("开始")
+        self.start_action.setText("start")
         self.start_action.setShortcut('F1')
         self.start_action.triggered.connect(self.one)
-
-        #停止键
         self.stop_action = QAction(self)
         icon3 = QIcon()
         icon3.addPixmap(QPixmap("img/stop.png"), QIcon.Normal, QIcon.Off)
         self.stop_action.setIcon(icon3)
-        self.stop_action.setText("停止")
+        self.stop_action.setText("sotp")
         self.stop_action.setShortcut('F3')
-        self.stop_action.setDisabled(True)  #开始时该按钮不可点击
+        self.stop_action.setDisabled(True)
         self.stop_action.triggered.connect(self.three)
-
-        #暂停键
         self.pause_action = QAction(self)
         p_icon = QIcon()
         p_icon.addPixmap(QPixmap("img/pause.png"), QIcon.Normal, QIcon.Off)
         self.pause_action.setIcon(p_icon)
-        self.pause_action.setText("暂停")
+        self.pause_action.setText("stop")
         self.pause_action.setShortcut('F2')
-        self.pause_action.setDisabled(True)  # 开始时该按钮不可点击
+        self.pause_action.setDisabled(True)
         self.pause_action.triggered.connect(self.two)
-
-        #重新开始键
         self.actionRestart = QAction(self)
         icon4 = QIcon()
         icon4.addPixmap(QPixmap("img/restart.png"), QIcon.Normal, QIcon.Off)
         self.actionRestart.setIcon(icon4)
-        self.actionRestart.setText("重新开始")
+        self.actionRestart.setText("restart")
         self.actionRestart.setShortcut('F4')
-        self.actionRestart.setDisabled(True)  # 开始时该按钮不可点击
+        self.actionRestart.setDisabled(True)
         self.actionRestart.triggered.connect(self.forew)
-
-        #更新数据键
         self.action_update = QAction(self)
         icon5 = QIcon()
         icon5.addPixmap(QPixmap("img/update.png"), QIcon.Normal, QIcon.Off)
         self.action_update.setIcon(icon5)
-        self.action_update.setText("继续更新")
+        self.action_update.setText("continue")
         self.action_update.setShortcut('F5')
         self.action_update.setDisabled(True)
         self.action_update.triggered.connect(
@@ -361,20 +311,20 @@ class windw(QMainWindow):
         )
         self.action_exit = QAction(self)
         self.action_exit.setCheckable(False)
-        self.action_exit.setText("退出")
+        self.action_exit.setText("quite")
         self.action_exit.triggered.connect(self.dadwwa)
         self.action_exit.setShortcut('ctrl+Q')
-        self.action_exit.setStatusTip('退出应用程序')
+        self.action_exit.setStatusTip('exit')
         self.mainToolBar.addAction(self.start_action)
         self.mainToolBar.addAction(self.pause_action)
         self.mainToolBar.addAction(self.stop_action)
         self.mainToolBar.addAction(self.actionRestart)
         self.mainToolBar.addAction(self.action_update)
-        self.comNum = QLabel('下载速度：')
-        self.baudNum = QLabel('上传速度:')
-        self.getSpeed = QLabel('收包速度：')
-        self.sendSpeed = QLabel('发包速度：')
-        self.netNic = QLabel('Welcome to WireWhale! ^ _ ^')
+        self.comNum = QLabel('downpackge')
+        self.baudNum = QLabel('uploadpackge')
+        self.getSpeed = QLabel('recive')
+        self.sendSpeed = QLabel('sent')
+        self.netNic = QLabel('Welcome to yukikaze!)
         self.statusBar.setStyleSheet("background: #EDEDED;")
         self.statusBar.addPermanentWidget(self.netNic, stretch=2)
         self.statusBar.addPermanentWidget(self.getSpeed, stretch=1)
